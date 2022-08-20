e9928989e677e3622be1891d3485363d6f54e205
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-08-20 17:42:33 +0200

    updated navigation styling

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -3,17 +3,19 @@ import NavigationLink from "./NavigationLink";
 import { VscCode, VscHome } from 'react-icons/vsc';
 import { GiKnifeFork } from 'react-icons/gi';
 import { BsPencil } from 'react-icons/bs';
+import { GrUserAdmin } from 'react-icons/gr';
 
 const Navigation = () => {
     return (
         <header>
-            <nav className='mx-auto mt-2 flex max-w-4xl items-center justify-between'>
+            <nav className='mx-auto flex max-w-5xl items-center justify-between pt-2'>
                 <span className="text-lg font-semibold">Jakub Abrahoim</span>
                 <div className='flex'>
                     <NavigationLink href='/' icon={<VscHome />} caption='Home' />
-                    <NavigationLink href='#' icon={<VscCode />} caption='Portfolio' />
+                    <NavigationLink href='/portfolio' icon={<VscCode />} caption='Portfolio' />
                     <NavigationLink href='#' icon={<GiKnifeFork />} caption='Cooking' />
                     <NavigationLink href='#' icon={<BsPencil />} caption='Blog' />
+                    <NavigationLink href='#' icon={<GrUserAdmin />} caption='Admin' />
                 </div>
             </nav>
         </header>
--- components/Navigation/NavigationLink.tsx
+++ components/Navigation/NavigationLink.tsx
@@ -12,12 +12,13 @@ interface Props {
 
 const NavigationLink = (props: Props) => {
     const { pathname } = useRouter();
+    const isRouteActive = pathname === props.href;
 
     return (
         <Link href={props.href}>
             <a className={
-                `mx-2 rounded-lg px-2 py-1 text-lg font-semibold transition hover:bg-gray-200
-                ${pathname === props.href && 'underline decoration-gray-400 underline-offset-2'}`
+                `mx-2 rounded-lg px-2 py-1 text-lg font-semibold transition hover:bg-stone-200
+                ${isRouteActive && 'underline decoration-gray-400 underline-offset-2'}`
             }>
                 <span className='flex items-center'>
                     <IconContext.Provider value={{ className: 'w-5 h-5 mr-1.5' }}>
