58a166b60fe856c49db44c90bb26e27c73409247
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-08-20 13:56:24 +0200

    updated nav styling

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,22 +1,28 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navigation = () => {
+    const { pathname } = useRouter();
+    const navLinkClass: string = 'px-2 py-1 mx-2 hover:bg-gray-200 rounded-lg';
+
     return (
         <header>
             <nav className='mx-auto mt-2 flex max-w-4xl justify-between'>
                 <span>Jakub Abrahoim</span>
                 <div>
                     <Link href='/'>
-                        <a className="px-2 py-1">Home</a>
+                        <a className={`${navLinkClass} ${pathname === '/' && 'underline decoration-gray-300 underline-offset-2'}`}>
+                            Home
+                        </a>
                     </Link>
-                    <Link href='/'>
-                        <a className="px-2 py-1">Portfolio</a>
+                    <Link href='#'>
+                        <a className={navLinkClass}>Portfolio</a>
                     </Link>
-                    <Link href='/'>
-                        <a className="px-2 py-1">Cooking</a>
+                    <Link href='#'>
+                        <a className={navLinkClass}>Cooking</a>
                     </Link>
-                    <Link href='/'>
-                        <a className="px-2 py-1">Blog</a>
+                    <Link href='#'>
+                        <a className={navLinkClass}>Blog</a>
                     </Link>
                 </div>
             </nav>
