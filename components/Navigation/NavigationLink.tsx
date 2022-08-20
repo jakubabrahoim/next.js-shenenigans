e7947d76b763b7ef09fd8fd8a5100822a6c184ac
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-08-20 19:22:52 +0200

    changed hover styling on nav links

--- components/Navigation/NavigationLink.tsx
+++ components/Navigation/NavigationLink.tsx
@@ -17,8 +17,8 @@ const NavigationLink = (props: Props) => {
     return (
         <Link href={props.href}>
             <a className={
-                `group mx-2 rounded-lg px-2 py-1 text-lg font-medium text-jet-200 transition hover:bg-stone-200
-                ${isRouteActive && 'underline decoration-gray-400 underline-offset-4'}`
+                `group mx-2 rounded-lg px-2 py-1 text-lg font-medium text-jet-200 transition duration-500 hover:bg-stone-200
+                ${isRouteActive && 'underline decoration-gray-400 underline-offset-4 hover:decoration-orchid-200'}`
             }>
                 <span className='flex items-center'>
                     <IconContext.Provider value={{ className: 'group-hover:text-orchid-200 w-5 h-5 mr-1.5' }}>
