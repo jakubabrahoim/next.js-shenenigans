a3225d9e37d4fded8d65442ff4e1b70dd2d6f6ef
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-08-21 12:10:24 +0200

    added login form styling

--- components/Admin/LoginForm.tsx
+++ components/Admin/LoginForm.tsx
@@ -1,25 +1,75 @@
+import { FormEvent, useState } from "react";
+
+import { IconContext } from "react-icons";
+import { MdAlternateEmail, MdLockOutline } from 'react-icons/md';
+import { AiOutlineEyeInvisible, AiOutlineEye } from 'react-icons/ai';
+
 const LoginForm = () => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+    
+    const inputFieldStyle: string = 'h-10 w-56 rounded-lg border px-6 align-middle outline-none focus:ring-1 focus:ring-orchid-200';
+    const emailPasswordIconStyle: string = 'w-4 h-4 ml-1 -mt-7 relative top-7 text-slate-400';
+    const showPasswordIconStyle: string = 'w-4 h-4 mr-1 -mb-7 relative bottom-7 left-9/10 text-slate-400 hover:text-orchid-200 transition duration-500';
+
+    const submitLoginForm = (event: FormEvent) => {
+        event.preventDefault();
+    }
+
     return (
         <div className='flex h-full w-screen items-center justify-center'>
-            <form>
-                <label className='mb-4 flex flex-col'>
-                    Username:
+            <form 
+                onSubmit={submitLoginForm}
+                className='absolute'
+            >
+                <label className='mb-7 flex flex-col'>
+                    <IconContext.Provider value={{ className: emailPasswordIconStyle }}>
+                        <MdAlternateEmail />
+                    </IconContext.Provider>
                     <input
-                        className='rounded-lg' 
-                        type='text' 
+                        className={inputFieldStyle}
+                        type='email'
+                        placeholder='E-mail'
                     />
                 </label>
                 <label className='mb-4 flex flex-col'>
-                    Password:
+                    <IconContext.Provider value={{ className: emailPasswordIconStyle }}>
+                        <MdLockOutline />
+                    </IconContext.Provider>
                     <input 
-                        className='rounded-lg'
-                        type='password' 
+                        className={inputFieldStyle}
+                        type={`${isPasswordVisible ? 'text' : 'password'}`}
+                        placeholder='Password'
                     />
+                    <button 
+                        onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                        aria-labelledby='showPassword'
+                    >
+                        {
+                            isPasswordVisible ?
+                            <IconContext.Provider 
+                                value={
+                                    { className: showPasswordIconStyle }
+                                }
+                            >
+                                <AiOutlineEyeInvisible />
+                            </IconContext.Provider>
+                            :
+                            <IconContext.Provider 
+                                value={
+                                    { className: showPasswordIconStyle }
+                                }
+                            >
+                                <AiOutlineEye />
+                            </IconContext.Provider>
+                        }
+                        <span id='showPassword' hidden>Show/Hide password</span>
+                    </button>
                 </label>
                 <input 
-                    className='cursor-pointer'
+                    className='h-7 w-full cursor-pointer rounded-lg bg-orchid text-white transition duration-500 hover:bg-orchid-200'
                     type='submit' 
                     name='Log in' 
+                    value='Log in'
                 />
             </form>
         </div>
