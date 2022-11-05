067856f36724b5c2ecae85ca3f9932421a5ccc85
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-11-05 21:35:06 +0100

    portfolio item styling and layout

--- components/Portfolio/PorfolioWrapper.tsx
+++ components/Portfolio/PorfolioWrapper.tsx
@@ -1,9 +1,14 @@
-import PortfolioItem from "./PortfolioItem";
+import { PortolioItem } from '../../types/types';
+import PortfolioItem from './PortfolioItem';
 
 const PortfolioWrapper = () => {
-    const projects = [
+    const projects: Array<PortolioItem> = [
         {
-            name: 'Website for IHProject s.r.o',
+            name: 'IH project s.r.o',
+            description: 'This project was a website for a company called IH project s.r.o. It was also my first website so it doesn\'t look that good 🙈.',
+            imageSrc: '/images/ihproject.png',
+            technologies: ['HTML', 'CSS', 'JavaScript', 'Bootstrap'],
+            websiteUrl: 'https://www.ihproject.sk/index.html',
         },
     ]
 
@@ -11,12 +16,8 @@ const PortfolioWrapper = () => {
         <div className='mx-auto mt-16 max-w-6xl'>
             <h1 className='mb-4 text-2xl font-semibold'>Some of the projects I worked on...</h1>
             <div className='grid grid-cols-1 justify-items-center gap-10 md:grid-cols-2 lg:grid-cols-3'>
-                <PortfolioItem />
-                <PortfolioItem />
-                <PortfolioItem />
-                <PortfolioItem />
-                <PortfolioItem />
-                <PortfolioItem />
+                <PortfolioItem projectInfo={projects[0]} />
+                
             </div>
         </div>
     )
--- components/Portfolio/PortfolioItem.tsx
+++ components/Portfolio/PortfolioItem.tsx
@@ -1,13 +1,56 @@
-const PortfolioItem = () => {
+import { AiOutlineLink, AiFillGithub } from 'react-icons/ai'
+import { PortolioItem } from '../../types/types';
+import ReactIcon from '../ReactIcon';
+
+interface Props {
+    projectInfo: PortolioItem;
+}
+
+const PortfolioItem = ({ projectInfo }: Props) => {
     return (
-        <div className="flex w-full flex-col rounded-lg border border-jet-200">
-            <div className="w-full">
-                Image placeholder
+        <article className='flex w-full flex-col rounded-lg border border-jet-200 transition-transform hover:scale-105 hover:shadow-xl'>
+            <div className='group relative mb-2 w-full rounded-t-lg'>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src={projectInfo.imageSrc} alt='IH project s.r.o' className='w-full rounded-t-lg group-hover:blur-xs' />
+                <div className='invisible absolute inset-0 z-50 flex h-full w-full items-center justify-center group-hover:visible'>
+                    {
+                        projectInfo.websiteUrl &&
+                            <a 
+                                href={projectInfo.websiteUrl} 
+                                className='rounded-full border border-jet-200 bg-orchid/10 p-2 transition duration-200 hover:border-orchid-200'
+                                target='_blank'
+                                rel='noreferrer'
+                            >
+                                <ReactIcon icon={<AiOutlineLink />} iconClass='w-8 h-8 text-jet-200 hover:text-orchid-200 transition duration-200' />
+                            </a>
+                    }
+                    {
+                        projectInfo.githubUrl &&
+                            <a 
+                                href={projectInfo.githubUrl}
+                                className='rounded-full border border-jet-200 bg-orchid/10 p-2 transition duration-200 hover:border-orchid-200'
+                                target='_blank'
+                                rel='noreferrer'
+                            >
+                                <ReactIcon icon={<AiFillGithub />} iconClass='w-8 h-8 text-jet-200 hover:text-orchid-200 transition duration-200' />
+                            </a>
+                    }
+                </div>
             </div>
-            <div className="px-2">
-                <h2 className="text-lg font-semibold">Project name</h2>
+            <div className='mb-1 flex flex-wrap gap-2 px-2'>
+                {
+                    projectInfo.technologies.map((technology, index) => (
+                        <span key={index} className='rounded-sm border border-jet-200 px-2 py-1 text-xs font-semibold'>
+                            {technology}
+                        </span>
+                    ))
+                }
             </div>
-        </div>
+            <div className='px-2 pb-2'>
+                <h2 className='mb-1 text-lg font-semibold underline underline-offset-4'>{projectInfo.name}</h2>
+                <p>{projectInfo.description}</p>
+            </div>
+        </article>
     )
 }
 
