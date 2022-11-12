6dd0afdb6f556be6a8e1d21f0f849410478aaf19
Jakub Abrahoim <jakub.abrahoim@windowslive.com>
2022-11-12 22:52:56 +0100

    added contact footer to portfolio

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,26 +1,38 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Footer from '../components/Footer/Footer';
 
 import Navigation from '../components/Navigation/Navigation';
 import ParticlesBackground from '../components/ParticlesBackground';
 import PortfolioWrapper from '../components/Portfolio/PorfolioWrapper';
 
 const Portfolio: NextPage = () => {
-	return (
-		<section className='min-h-screen bg-alabaster px-4 font-montserrat text-jet-200 xl:px-0'>
-			<Head>
-				<meta charSet='utf-8' />
-				<meta name='author' content='Jakub Abrahoim' />
-				<meta name='keywords' content='Jakub Abrahoim, Portfolio, Radio Stream' />
-				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
-				<link rel='icon' type='image/x-icon' href='/images/portfolioFavicon.ico' />
-				<title>Portfolio</title>
-			</Head>
-			<ParticlesBackground />
+    return (
+        <section className='min-h-screen bg-alabaster px-4 font-montserrat text-jet-200 xl:px-0'>
+            <Head>
+                <meta charSet='utf-8' />
+                <meta name='author' content='Jakub Abrahoim' />
+                <meta
+                    name='keywords'
+                    content='Jakub Abrahoim, Portfolio, Radio Stream'
+                />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1.0'
+                />
+                <link
+                    rel='icon'
+                    type='image/x-icon'
+                    href='/images/portfolioFavicon.ico'
+                />
+                <title>Portfolio</title>
+            </Head>
+            <ParticlesBackground />
             <Navigation />
-			<PortfolioWrapper />
-		</section>
-	)
-}
+            <PortfolioWrapper />
+            <Footer />
+        </section>
+    );
+};
 
 export default Portfolio;
